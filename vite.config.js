a41a6adaf90f1b4e5ada1998540e2419c2ed0e99
Enrique Caamal <enriquemaster154@gmail.com>
2023-12-02 08:48:20 -0600

    Primer commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https:///Enriquemaster.github.io/API_SITMOTUL",
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
